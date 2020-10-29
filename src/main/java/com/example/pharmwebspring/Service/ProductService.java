fbea6ab9b8d57c2537f7631916101db743981de4
yeji <yeji3841@naver.com>
2020-10-30 01:28:06 +0900

    map api success && time && product db

---
+++ src/main/java/com/example/pharmwebspring/Service/ProductService.java
@@ -0,0 +1,12 @@
+package com.example.pharmwebspring.Service;
+
+import java.util.List;
+import com.example.pharmwebspring.Model.Product;
+public interface ProductService {
+    List<Product> listProduct();
+    //Product detailProduct(String prod_name);
+    //String fileInfo(String prod_name);
+    //void updateProduct(Product product);
+    //void deleteProduct(String prod_name);
+    //void insertProduct(Product product);
+}
